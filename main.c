b6724ffffc1a9b8bfbdcfb1eccae4d1c972e9a8b
NasaTigyr <nikolay.ts.stefanov@gmail.com>
2026-03-31 10:48:17 +0300

    Made the new inserted piece print on the game table. Now is time for falling.

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h> 
+#include <stdlib.h> 
 #include <termios.h> 
 #include <unistd.h> 
+#include <time.h> 
 
 static struct termios term; 
 static char flag = '1'; 
 
-int Block[4][4] = {
+#define H 22
+#define W 12
+
+int gametable[H][W]; 
+int tekushta[4][4]; 
+
+int B[4][4] = {
   {0,0,0,0},
   {0,1,1,0},
   {0,1,1,0},
@@ -13,72 +21,47 @@ int Block[4][4] = {
 };
 
 int Line[4][4] = {
-  {0,1,0,0},
-  {0,1,0,0},
-  {0,1,0,0},
-  {0,1,0,0}
+  {0,0,0,0},
+  {1,1,1,1},
+  {0,0,0,0},
+  {0,0,0,0}
 };
 
-int Spiece[4][4] = {
-  {0,1,0,0},
+int S[4][4] = {
+  {0,0,0,0},
   {0,1,1,0},
+  {1,1,0,0},
+  {0,0,0,0}
+};
+
+int Z[4][4] = {
+  {0,0,0,0},
+  {0,1,1,0},
+  {0,0,1,1},
+  {0,0,0,0}
+};
+
+int J[4][4] = {
+  {0,0,0,0},
+  {1,1,1,0},
   {0,0,1,0},
   {0,0,0,0}
 };
 
-int Zpiece[4][4] = {
-  {0,0,1,0},
-  {0,1,1,0},
+int L[4][4] = {
+  {0,0,0,0},
+  {0,1,1,1},
   {0,1,0,0},
   {0,0,0,0}
 };
 
-int Jpiece[4][4] = {
-  {0,0,1,0}, 
+int T[4][4] = {
+  {0,0,0,0},
+  {0,1,1,1},
   {0,0,1,0},
-  {0,1,1,0},
   {0,0,0,0}
 };
 
-int Lpiece[4][4] = {
-  {0,1,0,0}, 
-  {0,1,0,0},
-  {0,1,1,0},
-  {0,0,0,0}
-};
-
-int gametable[21][12] = {
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1}
-}; 
-
-void frame() {
-  for(int i = 0; i <=20; i++) {
-    for( int j = 0; j <=11; j++) {
-      ( gametable[i][j] == 1) ? putchar('#') : putchar(' ') ; 
-    }
-    printf("\n"); 
-  }
-}
 
 void enableRawMode() { 
   struct termios raw = term; 
@@ -89,9 +72,121 @@ void enableRawMode() {
   tcsetattr(0, TCSAFLUSH, &raw); 
 } 
 
-  void atexit() { 
-    tcsetattr(0, TCSAFLUSH, &term); 
+void gametable_init() { 
+  for(int i = 0; i < H; i++) {
+    for(int j = 0; j < W; j++) {
+      if(i == 0 || i == H-1) { 
+        gametable[i][j] = 2;
+      } else if(j == 0 || j == W-1) { 
+        gametable[i][j] = 3;
+      } else { 
+        gametable[i][j] = 0;
+      }
+    }
   }
+}
+  
+void draw() {
+  for(int i = 0; i < H; i++) {
+    for( int j = 0; j < W; j++) {
+//      printf("%d", gametable[i][j]);
+      switch(gametable[i][j]) {
+        case 0: printf(" ");break;
+        case 1: printf("#");break;
+        case 2: printf("-");break;
+        case 3: printf("!");break;
+      }
+    }
+    printf("\n"); 
+  }
+}
+
+void printtekushta( int neshto[4][4]) { 
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
+      printf("%d", neshto[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void otmestvane(char key, char flag) {
+  if(flag == 0) {
+//    int num = rand()%7;
+    srand(time(NULL));
+    int num = rand()%7;
+    printf("This is the rand num: %d", num);
+    switch(num) {
+      case 0: 
+        for(int i = 0; i < 4; i++) { 
+          for(int j = 0; j < 4; j++) { 
+           tekushta[i][j] = B[i][j]; 
+          }
+        }
+        printf("This is case0\n");
+      break;
+      case 1: 
+        for(int i = 0; i < 4; i++) { 
+          for(int j = 0; j < 4; j++) { 
+           tekushta[i][j] = Line[i][j]; 
+          }
+        }
+        printf("This is case1\n");
+      break;
+      case 2: 
+        for(int i = 0; i < 4; i++) { 
+          for(int j = 0; j < 4; j++) { 
+           tekushta[i][j] = T[i][j]; 
+          }
+        }
+        printf("This is case2\n");
+      break;
+      case 3: 
+        for(int i = 0; i < 4; i++) { 
+          for(int j = 0; j < 4; j++) { 
+           tekushta[i][j] = S[i][j]; 
+          }
+        }
+        printf("This is case3\n");
+      break;
+      case 4: 
+        for(int i = 0; i < 4; i++) { 
+          for(int j = 0; j < 4; j++) { 
+           tekushta[i][j] = Z[i][j]; 
+          }
+        }
+        printf("This is case4\n");
+      break;
+      case 5: 
+        for(int i = 0; i < 4; i++) { 
+          for(int j = 0; j < 4; j++) { 
+           tekushta[i][j] = L[i][j]; 
+          }
+        }
+        printf("This is case5\n");
+      break;
+      case 6: 
+        for(int i = 0; i < 4; i++) { 
+          for(int j = 0; j < 4; j++) { 
+           tekushta[i][j] = Z[i][j]; 
+          }
+        }
+        printf("This is case6\n");
+      break;
+    }
+      
+//     printtekushta(tekushta);
+  
+    int x = W/2 - 1;
+      for(int i = 0; i < 4; i++) { 
+        for(int j = 0; j < 4; j++) { 
+//          printf("This is i: %d and this is j: %d\n", i, j);
+         gametable[i+1][x + j] = tekushta[i][j]; 
+        }
+      }
+  }
+}
+
 char get_key() { 
   char c = 0; 
   read(STDIN_FILENO, &c, 1); 
@@ -99,37 +194,32 @@ char get_key() {
   return c; 
 }
 
-void update(char key) { 
-    switch(key) { 
-      case 'a': printf("This is the left button\r\n"); break;
-      case 'd': printf("This is the right button\r\n"); break;
-      case 's': printf("This is the down button\r\n"); break; 
-      case 'q': printf("This is the quit button\r\n"); flag = '0'; break; 
-    }
-}
-
 int main() { 
 
   tcgetattr(STDIN_FILENO, &term); 
-  atexit(); 
   setvbuf(stdout, NULL, _IONBF, 0); 
   enableRawMode(); 
-
   printf("\x1b[2J"); 
-  while(flag == '1') { 
+
+  char flag = 0;
+  char run = 1;
+  gametable_init(); 
+
+  while(run == 1) { 
 
   printf("\x1b[2J"); 
   printf("\033[H"); 
 //  printf("This is the loop\r\n");
 
     char key = get_key(); 
-    update(key);  
-    frame(); 
+    otmestvane(key, flag);
+    draw(); 
 
    // printf("this is the key pressed: %c\r\n", key);
     
-   
+     
    usleep(100000); 
+//   run = 0;
   }
 
 
